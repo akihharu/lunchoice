dab5e789c09c36f2a3205d936542761a389e8845
akiharu <kobayashihannter@gmail.com>
2026-01-15 11:01:47 +0900

    画面遷移の調整とログイン時の動線非表示

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_controller.default_url_options = { host: 'www.lunchoice.com' }
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
